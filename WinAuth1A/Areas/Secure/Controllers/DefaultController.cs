bd6edc12e998d34fee55d67fff21341f0c6e1afe
jonesj <justin.jones@lumiinsight.com>
2017-03-24 11:34:00 +0000

    Initial Commit

---
+++ WinAuth1A/Areas/Secure/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WinAuth1A.Areas.Secure.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Secure/Default
+		[Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	}
+}
---
+++ WinAuth1A/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WinAuth1A.MvcApplication" Language="C#" %>
